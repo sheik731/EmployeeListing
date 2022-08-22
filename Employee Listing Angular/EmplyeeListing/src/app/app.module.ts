c7f4e16bac324b08154d9e5d5dcea236434fd38e
sheik731 <98314037+sheik731@users.noreply.github.com>
2022-08-22 11:55:18 +0530

    Updated Angular

--- Employee Listing Angular/EmplyeeListing/src/app/app.module.ts
+++ Employee Listing Angular/EmplyeeListing/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
